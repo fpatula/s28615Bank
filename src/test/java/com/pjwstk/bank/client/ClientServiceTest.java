d3a635360d78fb8c4d527d0ed80235b8754ca601
fpatula <fpatula@gmail.com>
2024-01-27 16:58:26 +0100

    Added transaction service tests

--- src/test/java/com/pjwstk/bank/client/ClientServiceTest.java
+++ src/test/java/com/pjwstk/bank/client/ClientServiceTest.java
@@ -7,9 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
---
+++ src/test/java/com/pjwstk/bank/transaction/TransactionServiceITest.java
@@ -0,0 +1,44 @@
+package com.pjwstk.bank.transaction;
+
+import com.pjwstk.bank.client.ClientRepository;
+import com.pjwstk.bank.client.ClientService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class TransactionServiceITest {
+
+    @Autowired
+    ClientService clientService;
+    @Autowired
+    ClientRepository clientRepository;
+    @Autowired
+    TransactionService transactionService;
+
+    @Test
+    void withdrawBalanceSuccessfully() {
+        String clientID = "00001";
+        String clientName = "Andrzej";
+        String clientSurname = "Bialy";
+        double clientBalance = 34555.3;
+        clientService.registerClient(clientID, clientName, clientSurname, clientBalance);
+        Transaction transaction = transactionService.withdrawBalance(clientID, 32000);
+        clientRepository.purgeClients();
+        assertEquals(2555.3, transaction.getBalance());
+    }
+    @Test
+    void addBalanceSuccessfully() {
+        String clientID = "00001";
+        String clientName = "Andrzej";
+        String clientSurname = "Bialy";
+        double clientBalance = 34555.3;
+        clientService.registerClient(clientID,clientName,clientSurname,clientBalance);
+        Transaction transaction = transactionService.addBalance(clientID, 4000);
+        clientRepository.purgeClients();
+        assertEquals(38555.3, transaction.getBalance());
+    }
+}
