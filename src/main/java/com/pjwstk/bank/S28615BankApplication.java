f7b0077862ac2b96cbc2782a91a426f86a921c9c
fpatula <fpatula@gmail.com>
2024-01-27 15:02:09 +0100

    Initial commit

---
+++ src/main/java/com/pjwstk/bank/S28615BankApplication.java
@@ -0,0 +1,13 @@
+package com.pjwstk.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S28615BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S28615BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pjwstk/bank/S28615BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjwstk.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S28615BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
